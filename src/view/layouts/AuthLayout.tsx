98ba622031defd6fcc9ec7a11f9bedb610fa4e0d
Caique Reis <caiquereiss@outlook.com>
2023-08-10 14:36:12 -0300

    feat: finally layout

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,8 +1,22 @@
-import { Outlet } from "react-router-dom";
-
+import { Outlet } from 'react-router-dom';
+import illustrationCar from '../../assets/illustration-car.png';
 
 export function AuthLayout() {
   return (
-    <Outlet />
+    <div className="flex w-full h-full">
+      <div className="w-full h-full flex items-center justify-center flex-col gap-16 lg:w-1/2">
+        <div className='w-full max-w-[504px] px-8'>
+
+          <Outlet />
+
+        </div>
+      </div>
+
+      <div className="bg-gray-0 h-full justify-center items-center p-8 relative hidden lg:flex">
+        <img
+          src={illustrationCar}
+          className="object-contain w-full h-full   select-none rounded-[32px]" />
+      </div>
+    </div>
   )
 }
