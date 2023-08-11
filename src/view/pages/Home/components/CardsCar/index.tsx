853efd3f0adc9bdd50081f889eed1b757f10a066
Caique Reis <caiquereiss@outlook.com>
2023-08-11 14:35:19 -0300

    fix: correction  css

--- src/view/pages/Home/components/CardsCar/index.tsx
+++ src/view/pages/Home/components/CardsCar/index.tsx
@@ -1,5 +1,6 @@
 import { formatCurrency } from '../../../../../app/utils/formatCurrency'
-import { StarFilledIcon } from '@radix-ui/react-icons'
+import { StarFilledIcon, OpenInNewWindowIcon } from '@radix-ui/react-icons'
+import { Link } from 'react-router-dom';
 
 interface CardsCardProps {
   car: {
@@ -22,30 +23,38 @@ interface CardsCardProps {
 
 export function CardsCard({ car, toggleFavorite, favorites }: CardsCardProps) {
   return (
-    <div
-      className="p-4 bg-white rounded-2xl h-[300px] flex flex-col justify-center items-center border-b-4 border-b-teal-950 mb-8">
-      <div className='flex items-center gap-2'>
-        <h3 className="text-lg font-semibold">{car.modelo}</h3>
-        <span> {car.marca}</span>
-        <p>{car.ano}</p>
+    <>
+      <Link to={`/info-car/${car.id}`}
+        className='bg-blue-900 hover:bg-blue-800  absolute right-0 px-2 h-9 rounded-2xl font-medium text-white transition-all flex items-center justify-center'
 
-      </div>
-      <img src={car.img} className="w-8/12 h-8/12" />
-      <p>{formatCurrency(car.preco)}</p>
-
-      <button
-        className="flex items-center"
-        onClick={() => toggleFavorite(car.id)}
       >
+        <OpenInNewWindowIcon className='w-6 h-6' />
+      </Link>
+      <div
+        className="p-4 bg-white rounded-2xl h-[300px] flex flex-col justify-center items-center border-b-4 border-b-teal-950 mb-8">
+        <div className='flex items-center gap-2'>
+          <h3 className="text-lg font-semibold">{car.modelo}</h3>
+          <span> {car.marca}</span>
+          <p>{car.ano}</p>
 
-        <StarFilledIcon
-          width={20}
-          height={20}
-          className={`mr-1 ${favorites.includes(car.id) ? 'text-yellow-500' : 'text-gray-400'}`}
+        </div>
+        <img src={car.img} className="w-8/12 h-8/12 2xl:w-7/12" />
+        <p>{formatCurrency(car.preco)}</p>
 
-        />
-        Favorito
-      </button>
-    </div >
+        <button
+          className="flex items-center"
+          onClick={() => toggleFavorite(car.id)}
+        >
+
+          <StarFilledIcon
+            width={20}
+            height={20}
+            className={`mr-1 ${favorites.includes(car.id) ? 'text-yellow-500' : 'text-gray-400'}`}
+
+          />
+          Favorito
+        </button>
+      </div >
+    </>
   )
 }
--- src/view/pages/Home/components/TopPageCarrousel/index.tsx
+++ src/view/pages/Home/components/TopPageCarrousel/index.tsx
@@ -10,8 +10,8 @@ export function TopPageCarrousel() {
   const { sliderState,
     setSliderState } = useTopPageCarrouselController()
   return (
-    <div id="top-cars" className="w-full h-full">
-      <h1 className="text-center font-bold text-3xl tracking-[1px] mt-10 2xl:mt:5 text-orange-400">TOP 3 CARROS MAIS ALUGADOS</h1>
+    <div id="top-cars" className="w-full h-full scroll-smooth md:scroll-auto ">
+      <h3 className="text-center font-bold text-3xl tracking-[1px] mt-10 2xl:mt:5 text-blue-900">TOP 3 CARROS MAIS ALUGADOS</h3>
       <div className="w-full">
         <Swiper
 
@@ -34,11 +34,15 @@ export function TopPageCarrousel() {
           {
             TOPCARS.map((car, index) => (
               <SwiperSlide key={index}>
-                <div className="p-4 flex flex-col items-center justify-center bg-gray-100 shadow-lg rounded-2xl h-full w-full border-b-4">
+                <div className="p-4 flex flex-col items-center justify-center bg-white bg-opacity-75 rounded-2xl h-full w-full border-b-4">
                   <div className="flex gap-2 lg:gap-10 lg:mb-20">
-                    <h1 className="text-black font-bold lg:text-3xl tracking-[-0.5px]">{car.marca}</h1>
-                    <h2 className="text-black font-bold lg:text-3xl tracking-[-0.5px]">{car.modelo}</h2>
-                    <h3 className="text-blue-500 font-bold lg:text-3xl tracking-[1px]">{formatCurrency(car.preco)}</h3>
+                    <h1 className="text-gray-700 font-bold lg:text-3xl tracking-[-0.5px]">{car.marca}</h1>
+                    <h2 className="text-gray-700 font-bold lg:text-3xl tracking-[-0.5px]">{car.modelo}</h2>
+                    <h3 className="text-blue-900 font-bold lg:text-3xl tracking-[1px]">
+                      {formatCurrency(car.preco)}
+                      <span className="text-gray-500 text-[9px] tracking-[-0.5px]">/ao mês</span>
+                    </h3>
+
                   </div>
                   <img className="h-1/2 w-1/2 " src={car.img} />
 
@@ -48,7 +52,7 @@ export function TopPageCarrousel() {
           }
         </Swiper >
       </div >
-      <div className="w-full">
+      <div className="w-full mt-24 lg:mt-10">
         <img className="w-full" src={wave} />
       </div>
     </div >
